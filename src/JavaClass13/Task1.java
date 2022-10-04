ae7e7dab43472f4ca1f1a88fed2d5fffe64841df
TarasH <Taras.hulyar@gmail.com>
2022-10-04 19:34:52 -0400

    HwClass14

--- src/JavaClass13/Task1.java
+++ src/JavaClass13/Task1.java
@@ -6,8 +6,8 @@ public class Task1 {
     public static void main(String[] args) {
        // Create a String that will hold a sentence. Write a program to get a new String without any spaces.
         String str = "I need to go for a walk";
-        String [] arr=str.split(" ");
-        System.out.println(Arrays.toString(arr));
+        str=str.replaceAll(" ", "");
+        System.out.println("str = " + str);
 
 
     }
